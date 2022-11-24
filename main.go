ef8e567cc1a6a1e3222fd339fe2a1cb474c3fa6a
Irnovi Albaweny <huiralb@gmail.com>
2022-11-24 14:48:54 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+    "fmt"
+    "net/http"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
-	}, nil
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html")
+    if r.URL.Path == "/" {
+        fmt.Fprint(w, "<h1>Selamat datang di skillplus</h1>")
+    } else if r.URL.Path == "/aboutus" {
+        fmt.Fprint(w, "<h1>Skillplus adalah web tutorial seputar informasi teknologi</h1>")
+    } else {
+        w.WriteHeader(http.StatusNotFound)
+        fmt.Fprint(w, "<h1>Halaman yang dicari tidak ditemukan</h1>")
+    }
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(handler)
+    http.HandleFunc("/", handlerFunc)
+    http.ListenAndServe(":3000", nil)
 }
